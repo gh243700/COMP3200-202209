51b01449821f2a97bf7239bed28e26478c8aba61
JEL97 <gh243700@gmail.com>
2022-09-11 21:50:39 +0900

    lab1_try01

---
+++ Lab1/Add.cpp
@@ -0,0 +1,7 @@
+#include "Add.h"
+
+namespace lab1 {
+	int Add(int a, int b) {
+		return a + b;
+	}
+}
--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -1,4 +1,13 @@
+#include "Add.h"
+#include <iostream>
+
+using namespace std;
+using namespace lab1;
+
 int main()
 {
+
+	cout << Add(1, 3) << endl;
+	
 
 }
