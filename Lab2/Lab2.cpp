1da41f83442197ea9593c675e62c80f02628d3e4
JEL97 <gh243700@gmail.com>
2022-09-12 19:26:14 +0900

    lab2_init

---
+++ Lab2/Lab2.cpp
@@ -0,0 +1,59 @@
+#include "Lab2.h"
+#include <iostream>
+#include <iomanip>
+#include <limits>
+
+using namespace std;
+
+namespace lab2
+{
+	void PrintIntegers(std::istream& in, std::ostream& out)
+	{
+		int num; 
+		string trash; 
+
+		out << setw(12) << "oct" << setw(12) << "dec" << setw(12) << "hex" << endl;
+		out << "------------ ---------- --------" << endl;
+
+		while (!in.eof()) {
+			in >> num;
+
+			if (in.fail()) {
+				in.clear();
+				in >> trash;
+				continue;
+			}
+
+			out << setw(12) << right << oct << uppercase << num;
+			out << setw(12) << right << dec << uppercase << num;
+			out << setw(12) << right << hex << uppercase << num;
+			out << endl;
+		}
+	}
+
+	void PrintMaxFloat(std::istream& in, std::ostream& out)
+	{
+		float num;
+		string trash; 
+		float max = std::numeric_limits<float>::min();
+
+
+		while (!in.eof()) {
+			in >> num;
+			
+			if (in.fail()) {
+				in.clear();
+				in >> trash;
+				continue;
+			}
+
+			if (max < num) {
+				max = num;
+			}
+
+			out << "     " << setw(15) << showpos << internal << showpoint << setprecision(3) << fixed << num << endl;
+		}
+
+		out << "max: " << setw(15) << showpos << internal << showpoint << setprecision(3) << fixed << max  << endl;
+	}
+}
