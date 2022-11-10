d5f208028dbcc9eb65bbe6600c90dd235537b4da
JEL97 <gh243700@gmail.com>
2022-11-10 21:20:34 +0900

    Lab8_init

---
+++ Lab8/FixedVector.h
@@ -0,0 +1,238 @@
+#pragma once
+#include <cstdint>
+#include <cmath>
+#include <cassert>
+#include <iostream>
+namespace lab8
+{
+	template <typename T, size_t N>
+	class FixedVector final
+	{
+	public:
+		FixedVector();
+		FixedVector(const FixedVector& rhs) = delete;
+		FixedVector operator=(const FixedVector& rhs) = delete;
+		~FixedVector() = default;
+		bool Add(const T& t);
+		bool Remove(const T& t);
+		T Get(unsigned int index) const;
+		T& operator[](unsigned int index);
+		int GetIndex(const T& t) const;
+		size_t GetSize() const;
+		size_t GetCapacity() const;
+
+
+	private:
+		T mVector[N];
+		size_t mCount;
+	};
+
+	template<typename T, size_t N>
+	FixedVector<T, N>::FixedVector()
+		: mCount(0)
+	{
+	}
+
+	template<typename T, size_t N>
+	bool FixedVector<T, N>::Add(const T& t)
+	{
+		if (mCount >= N)
+		{
+			return false;
+		}
+
+		mVector[mCount++] = t;
+		return true;
+	}
+
+	template<typename T, size_t N>
+
+	bool FixedVector<T, N>::Remove(const T& t)
+	{
+		size_t index = mCount;
+		bool bRemoved = false;
+		for (size_t i = 0; i < mCount; ++i)
+		{
+			if (mVector[i] == t)
+			{
+				index = i;
+				bRemoved = true;
+				--mCount;
+				break;
+			}
+		}
+
+		for (size_t i = index; i < mCount; ++i)
+		{
+			mVector[i] = mVector[i + 1];
+		}
+
+		return bRemoved;
+	}
+
+	template<typename T, size_t N>
+	T FixedVector<T, N>::Get(unsigned int index) const
+	{
+		return mVector[index];
+	}
+
+	template<typename T, size_t N>
+	T& FixedVector<T, N>::operator[](unsigned int index)
+	{
+		return mVector[index];
+	}
+
+	template<typename T, size_t N>
+	int FixedVector<T, N>::GetIndex(const T& t) const
+	{
+		for (size_t i = 0; i < mCount; ++i)
+		{
+			if (mVector[i] == t)
+			{
+				return i;
+			}
+		}
+
+		return -1;
+	}
+
+	template<typename T, size_t N>
+	size_t FixedVector<T, N>::GetSize() const
+	{
+		return mCount;
+	}
+
+	template<typename T, size_t N>
+	size_t FixedVector<T, N>::GetCapacity() const
+	{
+		return N;
+	}
+
+	typedef uint32_t int_t;
+	template <size_t N>
+	class FixedVector<bool, N>
+	{
+	public:
+		FixedVector()
+			: mCount(0)
+		{
+			memset(mVector, 0, sizeof(int_t) * ((N + X) / BIT));
+		}
+
+		FixedVector(const FixedVector& rhs) = delete;
+		FixedVector operator=(const FixedVector& other) = delete;
+		~FixedVector() = default;
+
+		bool Add(const bool& t)
+		{
+			if (mCount >= N)
+			{
+				return false;
+			}
+
+			mVector[mCount / BIT] |= static_cast<int_t>(t) << (mCount % BIT);
+
+			++mCount;
+			return true;
+		}
+
+		bool Remove(const bool& t)
+		{
+
+			bool bRemoved = false;
+			int_t index = 0;
+			while (index < mCount)
+			{
+				if (static_cast<bool>(mVector[index / BIT] & 0x1 << (index % BIT)) == t)
+				{
+					--mCount;
+					bRemoved = true;
+					break;
+				}
+				++index;
+			}
+
+			if (!bRemoved)
+			{
+				return false;
+			}
+
+			int_t& n = mVector[index / BIT];
+			int_t back = n & static_cast<int_t>(std::pow(2, index % BIT) - 1);
+			n = n >> (index % BIT + 1);
+			n = n << (index % BIT);
+			n |= back;
+
+
+			Print();
+			index /= BIT;
+			for (unsigned int i = index; i < mCount / BIT; ++i)
+			{
+				mVector[i] &= 0x7FFFFFFF;
+				mVector[i] = mVector[i] | ((mVector[i + 1] & 0x1) << X);
+				mVector[i + 1] >>= 1;
+			}
+			Print();
+
+			return true;
+		}
+
+		bool Get(int_t index) const
+		{
+			return mVector[index / BIT] & 0x1 << (index % BIT);
+		}
+
+		bool operator[](unsigned int index) const
+		{
+			return Get(index);
+		}
+
+		int GetIndex(const bool t) const
+		{
+			unsigned int index = 0;
+			while (index < mCount)
+			{
+				int_t b = mVector[index / 2];
+				if ((b & 0x1 << index % BIT) == static_cast<int>(t))
+				{
+					return index;
+				}
+
+				++index;
+			}
+
+			return -1;
+		}
+
+		size_t GetSize() const
+		{
+			return mCount;
+		}
+		size_t GetCapacity() const
+		{
+			return N;
+		}
+
+		void Print() const
+		{
+			for (unsigned int i = 0; i < 49; ++i)
+			{
+				if (i % 32 == 0)
+				{
+					std::cout << std::endl;
+				}
+				bool b = mVector[i / BIT] & 0x1 << (i % BIT);
+				std::cout << b << ",";
+			}
+
+			std::cout << "---------------------------------------------------------------\n\n" << std::endl;
+		}
+
+	private:
+		enum { BIT = 32, X = 31 };
+		int_t mVector[(N + X) / BIT];
+		size_t mCount;
+
+	};
+
+}
--- Lab8/main.cpp
+++ Lab8/main.cpp
@@ -1,4 +1,324 @@
+﻿#include "FixedVector.h"
+#include <cassert>
+#include <iostream>
+#include <cstdint>
+
+using namespace lab8;
+
+void TEST1()
+{
+	{
+		FixedVector<int, 5> v1;
+		assert(v1.Remove(1) == false);
+		assert(v1.GetCapacity() == 5);
+
+		for (unsigned int i = 0; i < v1.GetCapacity(); ++i)
+		{
+			assert(v1.Add(i));
+		}
+
+		assert(v1.GetSize() == v1.GetCapacity());
+		assert(v1.Add(-1) == false);
+
+
+		assert(v1.Remove(0) == true);
+		assert(v1.GetSize() == 4);
+		assert(v1.Get(0) == 1);
+		assert(v1.Get(1) == 2);
+		assert(v1.Get(2) == 3);
+		assert(v1.Get(3) == 4);
+
+
+
+		assert(v1.Remove(2) == true);
+		assert(v1.GetSize() == 3);
+		assert(v1.Get(0) == 1);
+		assert(v1.Get(1) == 3);
+		assert(v1.Get(2) == 4);
+
+		assert(v1.Remove(4) == true);
+		assert(v1.GetSize() == 2);
+		assert(v1.Get(0) == 1);
+		assert(v1.Get(1) == 3);
+
+		assert(v1[0] == 1);
+		v1[0] = 4;
+		assert(v1[0] == 4);
+		assert(v1.GetIndex(4) == 0);
+		assert(v1.GetIndex(3) == 1);
+	}
+	{
+		FixedVector<int, 33> v1;
+		assert(v1.GetCapacity() == 33);
+
+		for (unsigned int i = 0; i < v1.GetCapacity(); ++i)
+		{
+			assert(v1.Add(i + 1));
+		}
+
+		assert(v1.GetSize() == v1.GetCapacity());
+		assert(v1.Add(-1) == false);
+
+
+		assert(v1.Remove(1) == true);
+		assert(v1.GetSize() == 32);
+		int expected[32];
+		for (unsigned int i = 0; i < 32; ++i)
+		{
+			expected[i] = i + 2;
+		}
+
+		for (unsigned int i = 0; i < 32; ++i)
+		{
+			assert(expected[i] == v1[i]);
+			assert(expected[i] == v1.Get(i));
+		}
+
+
+		assert(v1.Remove(3) == true); //2 4
+		assert(v1.GetSize() == 31);
+		assert(v1[0] == 2);
+		assert(v1[1] == 4);
+		assert(v1[2] == 5);
+
+
+		assert(v1.Remove(32) == true);
+		assert(v1.GetSize() == 30);
+		assert(v1.Get(29) == 33);
+		assert(v1.Get(28) == 31);
+		assert(v1.Remove(100) == false);
+		assert(v1.GetIndex(100) == -1);
+	}
+
+
+}
+
 int main()
 {
+	TEST1();
 
+
+	{
+		FixedVector<bool, 5> v1;
+		assert(v1.Remove(true) == false);
+		assert(v1.GetCapacity() == 5);
+
+		v1.Add(true);
+		v1.Add(false);
+		v1.Add(true);
+		v1.Add(false);
+		v1.Add(true);
+
+
+
+
+		assert(v1.GetSize() == v1.GetCapacity());
+		assert(v1.Add(-1) == false);
+
+
+		assert(v1.Remove(true) == true);
+		assert(v1.GetSize() == 4);
+		assert(v1.Get(0) == false);
+		assert(v1.Get(1) == true);
+		assert(v1.Get(2) == false);
+		assert(v1.Get(3) == true);
+
+
+
+		assert(v1.Remove(1) == true); // f f t
+		assert(v1.GetSize() == 3);
+		assert(v1.Get(0) == false);
+		assert(v1.Get(1) == false);
+		assert(v1.Get(2) == true);
+
+		assert(v1.Remove(4) == true);
+		assert(v1.GetSize() == 2);
+		assert(v1.Get(0) == false);
+		assert(v1.Get(1) == false);
+
+		assert(v1[0] == false);
+		assert(v1[0] == false);
+		assert(v1.GetIndex(false) == 0);
+		assert(v1.GetIndex(true) == -1);
+	}
+
+
+	{
+		FixedVector<bool, 16> v1;
+		assert(v1.GetCapacity() == 16);
+
+		for (unsigned int i = 0; i < v1.GetCapacity(); ++i)
+		{
+			assert(v1.Add(i % 2 == 0 ? true : false));
+		}
+
+		assert(v1.GetSize() == v1.GetCapacity());
+		//assert(v1.Add(-1) == false);
+
+		assert(v1.Remove(1) == true);
+		{
+			int expected[15] = {
+				false, true, false, true, false, true, false, true, false, true,
+				false, true, false, true, false
+			};
+
+			for (unsigned int i = 0; i < 15; ++i)
+			{
+				assert(expected[i] == v1[i]);
+				assert(expected[i] == v1.Get(i));
+			}
+
+
+		}
+
+	}
+
+	{
+		FixedVector<bool, 51> v1;
+		for (unsigned int i = 0; i < 40; ++i)
+		{
+			v1.Add(false);
+		}
+
+		v1.Add(true);
+		v1.Add(true);
+		v1.Add(false);
+		v1.Add(true);
+		v1.Add(false);
+		v1.Add(false);
+
+		v1.Print();
+
+		assert(v1.Remove(false) == true);
+
+		bool expected[50] =
+		{
+			false, false, false, false,	 false,	 false, false,	 false, false,
+			false, false, false, false, false,	 false,	 false, false,	 false, false,
+			false, false, false, false, false,	 false,	 false, false,	 false, false,
+			false, false, false, false, false,	 false,	 false, false,	 false, false,
+			true, true, false, true, false, false
+		};
+
+
+
+		for (unsigned int i = 0; i < 50; ++i)
+		{
+			assert(expected[i] == v1[i]);
+			assert(expected[i] == v1.Get(i));
+		}
+
+
+		assert(v1.Remove(true) == true);
+
+		bool expected1[49] =
+		{
+			false, false, false, false,	 false,	 false, false,	 false, false,
+			false, false, false, false, false,	 false,	 false, false,	 false, false,
+			false, false, false, false, false,	 false,	 false, false,	 false, false,
+			false, false, false, false, false,	 false,	 false, false,	 false, false,
+			true, false, true, false, false
+		};
+		v1.Print();
+		for (unsigned int i = 0; i < 49; ++i)
+		{
+			if (i % 10 == 0)
+			{
+				std::cout << std::endl;
+			}
+			std::cout << v1[i] << ",";
+
+			assert(expected1[i] == v1[i]);
+			assert(expected1[i] == v1.Get(i));
+		}
+
+	}
+
+
+	{
+		FixedVector<bool, 51> v1;
+		for (unsigned int i = 0; i < 32; ++i)
+		{
+			v1.Add(false);
+		}
+
+		v1.Add(true);
+		for (unsigned int i = 0; i < 8; ++i)
+		{
+			v1.Add(false);
+		}
+		v1.Add(true);
+		v1.Add(true);
+		v1.Add(false);
+		v1.Add(true);
+		v1.Add(false);
+		v1.Add(false);
+
+		assert(v1.Remove(true) == true);
+
+		bool expected1[49] =
+		{
+			false, false, false, false,	 false,	 false, false,	 false, false, false,
+			false, false, false, false, false,	 false,	 false, false,	 false, false,
+			false, false, false, false, false,	 false,	 false, false,	 false, false,
+			false, false, false, false, false,	 false,	 false, false,	 false, false,
+			true, true, false, true, false, false
+		};
+		v1.Print();
+		for (unsigned int i = 0; i < 49; ++i)
+		{
+			assert(expected1[i] == v1[i]);
+			assert(expected1[i] == v1.Get(i));
+		}
+
+	}
+
+	{
+		{
+			FixedVector<bool, 51> v1;
+			for (unsigned int i = 0; i < 31; ++i)
+			{
+				v1.Add(false);
+			}
+			v1.Add(true);
+
+			v1.Add(true);
+			for (unsigned int i = 0; i < 8; ++i)
+			{
+				v1.Add(false);
+			}
+			v1.Add(true);
+			v1.Add(true);
+			v1.Add(false);
+			v1.Add(true);
+			v1.Add(false);
+			v1.Add(false);
+
+			assert(v1.Remove(true) == true);
+
+			bool expected1[49] =
+			{
+				false, false, false, false,	 false,	 false, false,	 false, false, false,
+				false, false, false, false, false,	 false,	 false, false,	 false, false,
+				false, false, false, false, false,	 false,	 false, false,	 false, false,
+				false, true, false, false, false,	 false,	 false, false,	 false, false,
+				true, true, false, true, false, false
+			};
+			v1.Print();
+			for (unsigned int i = 0; i < 49; ++i)
+			{
+				assert(expected1[i] == v1[i]);
+				assert(expected1[i] == v1.Get(i));
+			}
+
+		}
+	
+	
+	
+	}
+
+
+
+	std::cout << "FIN";
+	system("pause");
 }
