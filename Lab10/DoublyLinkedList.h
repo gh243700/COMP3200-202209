f8af29cc001d5a2615278f1de8661e48f6ed611a
JEL97 <gh243700@gmail.com>
2022-11-22 19:27:33 +0900

    lab10_init

---
+++ Lab10/DoublyLinkedList.h
@@ -0,0 +1,206 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+#include <iostream>
+
+namespace lab10
+{
+	template<typename T>
+	class Node;
+
+	template<typename T>
+	class DoublyLinkedList
+	{
+	public:
+		DoublyLinkedList();
+		void Insert(std::unique_ptr<T> data);
+		void Insert(std::unique_ptr<T> data, unsigned int index);
+		bool Delete(const T& data);
+		bool Search(const T& data) const;
+		bool ForTestPerpose(std::vector<T> arr);
+
+		std::shared_ptr<Node<T>> operator[](unsigned int index) const;
+		unsigned int GetLength() const;
+	private:
+		std::shared_ptr<Node<T>> mHead;
+		size_t mCount;
+	};
+
+	template<typename T>
+	DoublyLinkedList<T>::DoublyLinkedList()
+		: mCount(0)
+	{
+	}
+
+	template<typename T>
+	void DoublyLinkedList<T>::Insert(std::unique_ptr<T> data)
+	{
+		++mCount;
+		if (mHead == nullptr)
+		{
+			mHead = std::make_shared<Node<T>>(std::move(data));
+			return;
+		}
+
+		std::shared_ptr<Node<T>> node = mHead;
+		while (node->Next != nullptr)
+		{
+			node = node->Next;
+		}
+
+		node->Next = std::make_shared<Node<T>>(std::move(data), node);
+
+	}
+
+	template<typename T>
+	void DoublyLinkedList<T>::Insert(std::unique_ptr<T> data, unsigned int index)
+	{
+		++mCount;
+		if (mHead == nullptr)
+		{
+			mHead = std::make_shared<Node<T>>(std::move(data));
+			return;
+		}
+
+		std::shared_ptr<Node<T>> node = mHead;
+		size_t i = 0;
+
+		while (i++ < index)
+		{
+			if (node->Next == nullptr)
+			{
+				std::shared_ptr<Node<T>> newNode = std::make_shared<Node<T>>(std::move(data), node);
+				newNode->Next = node->Next;
+				node->Next = newNode;
+				return;
+			}
+			node = node->Next;
+		}
+
+		if (node == mHead)
+		{
+			std::shared_ptr<Node<T>> newNode = std::make_shared<Node<T>>(std::move(data));
+			newNode->Next = mHead;
+			mHead->Previous = newNode;
+			mHead = newNode;
+
+			return;
+		}
+
+		std::shared_ptr<Node<T>> newNode = std::make_shared<Node<T>>(std::move(data), node);
+		newNode->Next = node->Next;
+		node->Next = newNode;
+		node->Next->Previous = newNode;
+	}
+
+	template<typename T>
+	bool DoublyLinkedList<T>::Delete(const T& data)
+	{
+		std::shared_ptr<Node<T>> node = mHead;
+		bool exists = false;
+		while (node->Next != nullptr)
+		{
+			if (*(node->Data) == data)
+			{
+				exists = true;
+				break;
+			}
+
+			node = node->Next;
+		}
+
+		if (!exists)
+		{
+			return false;
+		}
+
+		--mCount;
+
+		if (node == mHead)
+		{
+			mHead = mHead->Next;
+			return true;
+		}
+
+		(node->Previous).lock()->Next = node->Next;
+
+		if (node->Next != nullptr)
+		{
+			node->Next->Previous = node->Previous;
+		}
+
+		return true;
+	}
+
+	template<typename T>
+	bool DoublyLinkedList<T>::Search(const T& data) const
+	{
+		std::shared_ptr<Node<T>> node = mHead;
+		while (node != nullptr)
+		{
+			if (*(node->Data) == data)
+			{
+				return true;
+			}
+
+			node = node->Next;
+		}
+
+		return false;
+	}
+
+	template<typename T>
+	std::shared_ptr<Node<T>> DoublyLinkedList<T>::operator[](unsigned int index) const
+	{
+		if (index >= mCount)
+		{
+			return nullptr;
+		}
+
+		std::shared_ptr<Node<T>> node = mHead;
+
+		for (int i = 0; i < index; ++i)
+		{
+			node = node->Next;
+		}
+
+		return node;
+	}
+
+	template<typename T>
+	unsigned int DoublyLinkedList<T>::GetLength() const
+	{
+		return mCount;
+	}
+
+
+	template<typename T>
+	bool DoublyLinkedList<T>::ForTestPerpose(std::vector<T> arr)
+	{
+		if (arr.size() != GetLength())
+		{
+			std::cout << "count not equal , mCount : " << mCount << ", vector count : " << arr.size() << std::endl;
+			return false;
+		}
+
+		std::shared_ptr<Node<T>> node = mHead;
+		unsigned int i = 0;
+		while (node != nullptr)
+		{
+			if (*(node->Data) != arr[i++])
+			{
+				std::cout << "not correct value at index : ";
+				std::cout << i - 1;
+				std::cout << "\n expected : ";
+				std::cout << arr[i - 1] << ", actural : " << (*(node->Data)) << std::endl;
+				return false;
+			}
+
+			node = node->Next;
+
+
+			return true;
+		}
+	}
+}
--- Lab10/main.cpp
+++ Lab10/main.cpp
@@ -1,4 +1,69 @@
+#include <memory>
+#include "Node.h"
+#include <cassert>
+#include <vector>
+#include "DoublyLinkedList.h"
+
+using namespace lab10;
+
 int main()
 {
+	std::unique_ptr<int> number1 = std::make_unique<int>(1);
+	std::unique_ptr<int> number2 = std::make_unique<int>(2);
+	std::unique_ptr<int> number3 = std::make_unique<int>(3);
+	std::unique_ptr<int> number4 = std::make_unique<int>(4);
+	std::unique_ptr<int> number5 = std::make_unique<int>(5);
+	std::unique_ptr<int> number6 = std::make_unique<int>(6);
+	std::unique_ptr<int> number7 = std::make_unique<int>(7);
+	std::unique_ptr<int> number8 = std::make_unique<int>(8);
+	std::unique_ptr<int> number9 = std::make_unique<int>(9);
+	
+	std::vector<int> expected = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
+	DoublyLinkedList<int> list;
+	list.Insert(std::move(number1));
+	list.Insert(std::move(number2));
+	list.Insert(std::move(number3));
+	list.Insert(std::move(number4));
+	list.Insert(std::move(number5));
+	list.Insert(std::move(number6));
+	list.Insert(std::move(number7));
+	list.Insert(std::move(number8));
+	list.Insert(std::move(number9));
+
+	list.ForTestPerpose(expected);
+
+	expected.erase(expected.begin());
+	list.Delete(1);
+	list.ForTestPerpose(expected);
+	
+	list.Insert(std::make_unique<int>(1), 0);
+	expected.insert(expected.begin(), 1);
+	list.ForTestPerpose(expected);
+
+	list.Delete(3);
+	expected.erase(expected.begin() + 2);
+	assert(list.ForTestPerpose(expected));
+
+	expected.insert(expected.begin() + 2, 3);
+	list.Insert(std::make_unique<int>(3), 2);
+	assert(list.ForTestPerpose(expected));
+
+
+	expected.push_back(10);
+	list.Insert(std::make_unique<int>(10), 9);
+	assert(list.ForTestPerpose(expected));
+
+	assert(list.Search(11) == false);
+	assert(list.Search(1) == true);
+	assert(list.Search(10) == true);
+
+	assert(*(list[0]->Data) == 1);
+	assert(*(list[9]->Data) == 10);
+	assert(list[11] == nullptr);
+
+	assert(list.GetLength() == 10);
+
+	std::cout << "dddddd";
+	system("pause");
 }
